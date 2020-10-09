553bf7ec2fa647d0be501b5b5d8aadbd3ab07c9c
maffreb <blandine.maffre07@gmail.com>
2020-10-09 15:44:54 +0200

    essaye

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ get_header();
         the_post(); ?>
 <article>
     <a href="<?php the_permalink();?>"><?php the_title();?></a>
+    <h2><?php the_title(); ?></h2>
     <div><?php the_content(); ?></div>
     <p><?php the_time();?></p>
     <p><?php the_author_posts_link();?></p>
