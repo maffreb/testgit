bfa021bd631c292781e2c5d0d02837da33a2a9f9
maffreb <blandine.maffre07@gmail.com>
2020-10-09 15:38:39 +0200

    fonction title

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ get_header();
         the_post(); ?>
 <article>
     <a href="<?php the_permalink();?>"><?php the_title();?></a>
-    <h2><?php the_title(); ?></h2>
     <div><?php the_content(); ?></div>
     <p><?php the_time();?></p>
     <p><?php the_author_posts_link();?></p>
