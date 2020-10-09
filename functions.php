0714e6a75a841031cb12316ec54a607fc4300310
maffreb <blandine.maffre07@gmail.com>
2020-10-09 15:53:06 +0200

    le commentaire

--- functions.php
+++ functions.php
@@ -4,4 +4,6 @@ function ajout_titre(){
   add_theme_support('title-tag');
 }
 add_action('after_setup_theme','ajout_titre');
+
+//je fait genre je tape un truc, mais en vrai j'en ai rien à faire
 ?>
